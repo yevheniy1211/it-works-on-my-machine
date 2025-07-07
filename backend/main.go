4c9a75e02c5c74ba6b14cc9f4aed5abe20440635
Ivanov <yevheniy.ivanov@amo.tech>
2025-07-07 19:46:00 +0300

    third lesson

--- backend/main.go
+++ backend/main.go
@@ -129,7 +129,7 @@ func httpError(err error, ctx *gin.Context, status int) {
 
 func corsMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization")
 		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
--- backend/video.go
+++ backend/video.go
@@ -13,6 +13,7 @@ import (
 	"github.com/go-pg/pg/v10"
 	"github.com/redis/go-redis/v9"
 	"gopkg.in/yaml.v3"
+	"crypto/tls"
 )
 
 var dbSession *pg.DB = nil
@@ -67,6 +68,7 @@ func getDB(c *gin.Context) *pg.DB {
 		User:     user,
 		Password: pass,
 		Database: name,
+		TLSConfig: &tls.Config{InsecureSkipVerify: true},
 	})
 	return dbSession
 }
@@ -206,5 +208,6 @@ func getRedis() (*redis.Client, error) {
 		Addr:     fmt.Sprintf("%s:%s", endpoint, port),
 		Password: "", // no password set
 		DB:       0,  // use default DB
+		TLSConfig: &tls.Config{InsecureSkipVerify: true},
 	}), nil
 }
